2ee8eaf4ed434d9d44e886c89427e3f6ea4ff8a1
zhengze <zhengze@sina.com.cn>
2020-04-14 17:18:46 +0800

    edit goroutine

--- 13_goroutine/main.go
+++ 13_goroutine/main.go
@@ -1,29 +1,24 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
-func loop1() {
-	var count int
-	for count < 100 {
-		count++
-		fmt.Println(count)
-		time.Sleep(time.Second)
-	}
+func product(c chan<- int, factory string) {
+    for i := 0; i < 10; i++ {
+        fmt.Println(factory, " send ", i)
+        c <- i
+    }
 }
 
-func loop2() {
-	var count int = 100
-	for {
-		count--
-		fmt.Println(count)
-		time.Sleep(time.Second)
-	}
+func consume(c <-chan int){
+    for j := 0; j < 10; j++ {
+        fmt.Println("get ", <-c)
+    }
 }
-
 func main() {
-	go loop1()
-	loop2()
+    ch1 := make(chan int, 10)
+    ch2 := make(chan int, 10)
+    go product(ch1, "factory1")
+    go product(ch2, "factory2")
+    consume(ch1)
+    consume(ch2)
 }
